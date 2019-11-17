631e157c17f9ecb32aa378ba43cfb04c441a1581
Damien Pontifex <damien.pontifex@gmail.com>
2019-11-17 17:36:19 +1100

    Notes on calling update cert from event grid trigger

--- functions/CertExpiryEventTrigger/__init__.py
+++ functions/CertExpiryEventTrigger/__init__.py
@@ -2,9 +2,15 @@ import json
 import logging
 
 import azure.functions as func
-
+from .azure_keyvault_letsencrypt import create_or_update_cert
+from .certs import certs
 
 def main(event: func.EventGridEvent):
+    """Respond to Event Grid events from Key Vault for:
+        Microsoft.KeyVault.CertificateNearExpiry",
+        Microsoft.KeyVault.CertificateExpired
+    """
+
     result = json.dumps({
         'id': event.id,
         'data': event.get_json(),
@@ -14,3 +20,8 @@ def main(event: func.EventGridEvent):
     })
 
     logging.info('Python EventGrid trigger processed an event: %s', result)
+
+    # TODO: Actually renew certificate - have to look up schema of event.get_json() object
+    # certificate_name = event.get_json()['name']
+    # domains = certs[certificate_name]
+    # create_or_update_cert(certificate_name, *domains)
